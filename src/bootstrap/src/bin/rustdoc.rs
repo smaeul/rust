cb97ef40ec507c7ff20f7c0857b1892a1946a0f3
Samuel Holland <samuel@sholland.org>
2017-12-02 17:25:44 -0600

    Fix rustdoc when cross-compiling on musl
    musl can't handle foreign-architecture libraries in LD_LIBRARY_PATH.

--- src/bootstrap/src/bin/rustdoc.rs
+++ src/bootstrap/src/bin/rustdoc.rs
@@ -7,12 +7,13 @@ use std::ffi::OsString;
 use std::path::PathBuf;
 use std::process::Command;
 
-use dylib_util::{dylib_path, dylib_path_var};
+use dylib_util::dylib_path_var;
 
 #[path = "../utils/bin_helpers.rs"]
 mod bin_helpers;
 
 #[path = "../utils/dylib.rs"]
+#[allow(dead_code)]
 mod dylib_util;
 
 fn main() {
@@ -29,9 +30,6 @@ fn main() {
     // is passed (a bit janky...)
     let target = args.windows(2).find(|w| &*w[0] == "--target").and_then(|w| w[1].to_str());
 
-    let mut dylib_path = dylib_path();
-    dylib_path.insert(0, PathBuf::from(libdir.clone()));
-
     let mut cmd = Command::new(rustdoc);
 
     if target.is_some() {
@@ -44,7 +42,7 @@ fn main() {
     }
 
     cmd.args(&args);
-    cmd.env(dylib_path_var(), env::join_paths(&dylib_path).unwrap());
+    cmd.env(dylib_path_var(), PathBuf::from(libdir.clone()));
 
     // Force all crates compiled by this compiler to (a) be unstable and (b)
     // allow the `rustc_private` feature to link to other unstable crates
@@ -78,7 +76,7 @@ fn main() {
         eprintln!(
             "rustdoc command: {:?}={:?} {:?}",
             dylib_path_var(),
-            env::join_paths(&dylib_path).unwrap(),
+            PathBuf::from(libdir.clone()),
             cmd,
         );
         eprintln!("sysroot: {sysroot:?}");
