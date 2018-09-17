7e50215a21b198c73fe575d28c847fd645c4ee40
Samuel Holland <samuel@sholland.org>
2018-09-17 02:29:06 +0000

    Add foxkit target specs

---
+++ compiler/rustc_target/src/spec/aarch64_foxkit_linux_musl.rs
@@ -0,0 +1,11 @@
+use crate::spec::Target;
+
+pub fn target() -> Target {
+    let mut base = super::aarch64_unknown_linux_musl::target();
+
+    base.llvm_target = "aarch64-foxkit-linux-musl".to_string();
+    base.vendor = "foxkit".to_string();
+    base.options.crt_static_default = false;
+
+    base
+}
---
+++ compiler/rustc_target/src/spec/armv7_foxkit_linux_musleabihf.rs
@@ -0,0 +1,11 @@
+use crate::spec::Target;
+
+pub fn target() -> Target {
+    let mut base = super::armv7_unknown_linux_musleabihf::target();
+
+    base.llvm_target = "armv7-foxkit-linux-musleabihf".to_string();
+    base.vendor = "foxkit".to_string();
+    base.options.crt_static_default = false;
+
+    base
+}
