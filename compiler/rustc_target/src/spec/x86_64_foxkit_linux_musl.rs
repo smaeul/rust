11e847dc0ab3f9e94709c24572ca53dc3cd33454
Samuel Holland <samuel@sholland.org>
2018-09-17 02:29:06 +0000

    Add foxkit target specs

---
+++ compiler/rustc_target/src/spec/x86_64_foxkit_linux_musl.rs
@@ -0,0 +1,11 @@
+use crate::spec::Target;
+
+pub fn target() -> Target {
+    let mut base = super::x86_64_unknown_linux_musl::target();
+
+    base.llvm_target = "x86_64-foxkit-linux-musl".to_string();
+    base.vendor = "foxkit".to_string();
+    base.options.crt_static_default = false;
+
+    base
+}
